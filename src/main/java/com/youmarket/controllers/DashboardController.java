d6c2e8732675e83469d2cf7c4c312fe8b131027a
TCL90 <acmedespliegue2@gmail.com>
2020-05-10 23:57:12 +0200

    Dashboard

--- src/main/java/com/youmarket/controllers/DashboardController.java
+++ src/main/java/com/youmarket/controllers/DashboardController.java
@@ -3,19 +3,14 @@ package com.youmarket.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.youmarket.configuration.security.CurrentUser;
 import com.youmarket.configuration.security.UserPrincipal;
-import com.youmarket.domain.Usuario;
-import com.youmarket.services.CestaService;
 import com.youmarket.services.DashboardService;
 
 @RestController
