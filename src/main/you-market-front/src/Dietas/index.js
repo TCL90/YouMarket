219695abda015d78a8514576eb6d856c34f2be83
fr4231gm <fr4231gm@gmail.com>
2020-05-12 20:04:31 +0200

    Fix

--- src/main/you-market-front/src/Dietas/index.js
+++ src/main/you-market-front/src/Dietas/index.js
@@ -11,7 +11,7 @@ function Dietas() {
   const [dietas, setDietas] = useState([]);
 
   const fetchDietas = useCallback(() => {
-    return fetch("https://youmarket-entrega5.herokuapp.com/list", {
+    return fetch("https://youmarket-entrega5.herokuapp.com/dieta/list", {
       headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
